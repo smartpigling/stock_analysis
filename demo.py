af523bf4f6421beba67fa3d23df52d8966a2043d
unknown <>
2016-12-01 23:05:07 +0800

    20161201

--- demo.py
+++ demo.py
@@ -10,18 +10,19 @@ tick = '600581'
 start_day = datetime.datetime.now() + datetime.timedelta(days=-3)
 dates = pd.date_range(start_day.strftime("%Y-%m-%d"), periods=3)
 
-
 trading_data = []
 for date in dates:
-    _df_trading_data = ts.get_tick_data(tick, date=date.strftime("%Y-%m-%d"))
-    if len(_df_trading_data.index) > 3 :
-        trading_data.append(_df_trading_data)
+    date = date.strftime("%Y-%m-%d")
+    df = ts.get_tick_data(tick, date=date)
+    if len(df.index) > 3 :
+        df['date'] = date
+        trading_data.append(df)
 df_trading_data = pd.concat(trading_data)
-
+df_trading_data.to_excel(r'D:\Workspace\CodeWorkspace\mydata.xlsx', sheet_name='mytest')
+print('Run Over!')
 #%%
-df = ts.get_tick_data(tick, date='2016-11-27')
-len(df.index)
-
+for res in ((lambda x: (x,x+1)) for x in range(10)):
+    print(res)
 #%%
 buy_vol_sum = df_trading_data[df_trading_data['type'] == '买盘']['volume'].sum()
 'buy volume:%s' % buy_vol_sum
